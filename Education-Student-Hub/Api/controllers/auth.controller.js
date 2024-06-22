224a0ca5ced9a61ac90a13a5625a7cefe6790a82
Deval Chovatia <deval.chovatia@gmail.com>
2024-06-22 14:56:50 -0400

    .

---
+++ Education-Student-Hub/Api/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+import User from "../models/user.model.js"
+import bcryptjs from "bcryptjs"
+
+export const signup = async(req, res) =>{
+    const { firstName, lastName, username, email, password } = req.body
+    const hashedPassword = bcryptjs.hashSync(password, 12)
+    const newUser = new User({ firstName, lastName, username, email, password: hashedPassword})
+    await newUser.save()
+    res.status(201).json("User created successfully")
+}
