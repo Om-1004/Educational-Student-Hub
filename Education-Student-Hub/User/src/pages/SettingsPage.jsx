8bbbcafcc1ff6a8891df48478db6555357d7837a
Deval Chovatia <deval.chovatia@gmail.com>
2024-06-23 14:36:30 -0400

    .

--- Education-Student-Hub/User/src/pages/SettingsPage.jsx
+++ Education-Student-Hub/User/src/pages/SettingsPage.jsx
@@ -118,108 +118,111 @@ export default function Settings({
 
           <div className="mt-5">
             <label className="mt-5 text-sm text-gray-800">Last Name</label>
-            <div className="flex gap-3 items-center">
-              <input
-                className="p-2 rounded-xl w-80 border-2 border-gray-300"
-                type="text"
-                id="lastName"
-                value={formData.lastName}
-                readOnly={!isEditing.lastName}
-                onChange={handleChange}
-              />
-              <button onClick={() => handleEdit("lastName")}>
-                {isEditing.lastName ? <Save size={18} /> : <Pencil size={18} />}
-              </button>
-            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="flex gap-3 items-center">
+                <input
+                  className="p-2 rounded-xl w-80 border-2 border-gray-300"
+                  type="text"
+                  id="lastName"
+                  value={formData.lastName}
+                  readOnly={!isEditing.lastName}
+                  onChange={handleChange}
+                />
+                <button onClick={() => handleEdit("lastName")}>
+                  {isEditing.lastName ? <Save size={18} /> : <Pencil size={18} />}
+                </button>
+              </div>
+            </form>
           </div>
           <div className="mt-5">
             <label className="mt-5 text-sm text-gray-800">Username</label>
-            <div className="flex gap-3 items-center">
-              <input
-                className="p-2 rounded-xl w-80 border-2 border-gray-300"
-                type="text"
-                id="username"
-                value={formData.username}
-                readOnly={!isEditing.username}
-                onChange={handleChange}
-              />
-              <button onClick={() => handleEdit("username")}>
-                {isEditing.username ? <Save size={18} /> : <Pencil size={18} />}
-              </button>
-            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="flex gap-3 items-center">
+                <input
+                  className="p-2 rounded-xl w-80 border-2 border-gray-300"
+                  type="text"
+                  id="username"
+                  value={formData.username}
+                  readOnly={!isEditing.username}
+                  onChange={handleChange}
+                />
+                <button onClick={() => handleEdit("username")}>
+                  {isEditing.username ? <Save size={18} /> : <Pencil size={18} />}
+                </button>
+              </div>
+            </form>
           </div>
 
           <div className="mt-5">
             <label className="mt-5 text-sm text-gray-800">Email</label>
-            <div className="flex gap-3 items-center">
-              <input
-                className="p-2 rounded-xl w-80 border-2 border-gray-300"
-
-                type="email"
-                name="email"
-                id="email"
-                value={formData.email}
-                readOnly={!isEditing.email}
-
-                onChange={handleChange}
-              />
-              <button onClick={() => handleEdit("email")}>
-                {isEditing.email ? <Save size={18} /> : <Pencil size={18} />}
-              </button>
-            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="flex gap-3 items-center">
+                <input
+                  className="p-2 rounded-xl w-80 border-2 border-gray-300"
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={formData.email}
+                  readOnly={!isEditing.email}
+                  onChange={handleChange}
+                />
+                <button onClick={() => handleEdit("email")}>
+                  {isEditing.email ? <Save size={18} /> : <Pencil size={18} />}
+                </button>
+              </div>
+            </form>
           </div>
 
           <div className="mt-5">
             <label className="mt-5 text-sm text-gray-800">Password</label>
-            <div className="flex gap-3 items-center">
-              <div className="relative">
-                <input
-                  className="p-2 rounded-xl w-80 border-2 border-gray-300"
-                  type={openPassword ? "text" : "password"}
-
-                  name="password"
-                  id="password"
-                  
-                  readOnly={!isEditing.password}
-
-                  onChange={handleChange}
-                />
-                <button
-                  onClick={handleEyeClick}
-                  className="absolute top-1/2 right-5 -translate-y-1/2"
-                >
-                  {openPassword ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="gray"
-                      className="bi bi-eye"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
-                      <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
-                    </svg>
-                  ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="gray"
-                      className="bi bi-eye-slash"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7 7 0 0 0-2.79.588l.77.771A6 6 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755q-.247.248-.517.486z" />
-                      <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829" />
-                      <path d="M3.35 5.47q-.27.24-.518.487A13 13 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7 7 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12z" />
-                    </svg>
-                  )}
+            <form onSubmit={handleSubmit}>
+              <div className="flex gap-3 items-center">
+                <div className="relative">
+                  <input
+                    className="p-2 rounded-xl w-80 border-2 border-gray-300"
+                    type={openPassword ? "text" : "password"}
+                    name="password"
+                    id="password"
+                    readOnly={!isEditing.password}
+                    onChange={handleChange}
+                  />
+                  <button
+                    onClick={handleEyeClick}
+                    className="absolute top-1/2 right-5 -translate-y-1/2"
+                  >
+                    {openPassword ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="gray"
+                        className="bi bi-eye"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
+                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="gray"
+                        className="bi bi-eye-slash"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7 7 0 0 0-2.79.588l.77.771A6 6 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755q-.247.248-.517.486z" />
+                        <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829" />
+                        <path d="M3.35 5.47q-.27.24-.518.487A13 13 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7 7 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12z" />
+                      </svg>
+                    )}
+                  </button>
+                </div>
+                <button onClick={() => handleEdit("password")}>
+                  {isEditing.password ? <Save size={18} /> : <Pencil size={18} />}
                 </button>
               </div>
-              <button onClick={() => handleEdit("password")}>
-                {isEditing.password ? <Save size={18} /> : <Pencil size={18} />}
-              </button>
-            </div>
+            </form>
           </div>
 
           <hr className="w-10/12 mt-8" />
